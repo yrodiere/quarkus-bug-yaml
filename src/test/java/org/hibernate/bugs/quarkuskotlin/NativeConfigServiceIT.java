62841a1935bd49830536f5c4b4e4ca1a24a6d2a1
Yoann Rodière <yoann@hibernate.org>
2020-08-31 11:30:21 +0200

    Create project
    mvn io.quarkus:quarkus-maven-plugin:1.7.1.Final:create \
      -DprojectGroupId=org.hibernate.bugs.quarkusyaml \
      -DprojectArtifactId=quarkus-yaml \
      -DprojectVersion=1.0.0-SNAPSHOT \
      -DclassName="org.hibernate.bugs.quarkuskotlin.ConfigService" \
      -Dextensions="config-yaml,resteasy-jsonb"

---
+++ src/test/java/org/hibernate/bugs/quarkuskotlin/NativeConfigServiceIT.java
@@ -0,0 +1,9 @@
+package org.hibernate.bugs.quarkuskotlin;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeConfigServiceIT extends ConfigServiceTest {
+
+    // Execute the same tests but in native mode.
+}
